1b88ab82ce13a385bd832ecb1c584af5b0c919b9
Jinxin Lin <jasonlin619207330@hotmail.com>
2018-03-08 22:44:15 +0000

    .
    .

---
+++ UI/Assets/scripts/quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class quit : MonoBehaviour {
+
+	public void quitgame(){
+		Debug.Log ("Quit Game.");
+		Application.Quit ();
+	}
+}
---
+++ UI/Assets/scripts/swap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swap : MonoBehaviour {
+
+	public void GotoPage(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+}
